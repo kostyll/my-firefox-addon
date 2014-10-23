1203c1fc9703d4d43d20b5aa4b43e7cb3748fd57
Andrew <andrew@vasyltsiv.esx.ua>
2014-10-23 17:53:35 +0300

    static files have been added

--- gen_popup.py
+++ gen_popup.py
@@ -4,9 +4,9 @@ from html import *
 def gen_popup_window():
     with HTML5 as out:
         with HEAD:
-            CSS(href='http://maxcdn.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css')
-            JS(src='http://code.jquery.com/jquery-git2.js')
-            JS(src='http://maxcdn.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js')
+            CSS(href='bootstrap.min.css')
+            JS(src='jquery-git2.js')
+            JS(src='bootstrap.min.js')
     with BODY:
         with DIV.container_fluid:
             with TEXTAREA:
